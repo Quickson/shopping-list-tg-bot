1e638f424b796de78b690a92cb6abb0ae2a2ea14
Quickson <quick625@yandex.ru>
2021-03-24 00:17:37 +0300

    db settings

--- models/base.py
+++ models/base.py
@@ -1,11 +1,11 @@
 from peewee import *
 import os
 
-# db = PostgresqlDatabase(os.environ['DB_NAME'], user=os.environ['DB_USER'], host=os.environ['DB_HOST'],
-#                         password=os.environ['DB_PASSWORD'])
+db = PostgresqlDatabase(os.environ['DB_NAME'], user=os.environ['DB_USER'], host=os.environ['DB_HOST'],
+                        password=os.environ['DB_PASSWORD'])
 
-db = PostgresqlDatabase('test', user='postgres', host='localhost',
-                        password='postgres')
+# db = PostgresqlDatabase('test', user='postgres', host='localhost',
+#                         password='postgres')
 
 
 class BaseModel(Model):
